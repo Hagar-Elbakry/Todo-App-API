0d42cadf4aa8c91d170bc666274e3b3dc9afb071
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-10-02 17:19:42 +0300

    add ForceJsonResponse middleware and enhance exception handling for JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +17,29 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (ModelNotFoundException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'Item Not Found'], 404);
+            }
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            if( $request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'Not Found'], 404);
+            }
+        });
+
+        $exceptions->renderable(function (AuthenticationException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'unAuthenticated'], 401);
+            }
+        });
+        $exceptions->renderable(function (ValidationException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'UnprocessableEntity', 'errors' => []], 422);
+            }
+        });
     })->create();

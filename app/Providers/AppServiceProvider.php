177f4d3a4c3ccdfe79f209b31ebaa666323746d4
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-10-08 21:27:33 +0300

    set token expire to 15 minutes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::enablePasswordGrant();
-        Passport::tokensExpireIn(CarbonInterval::minutes());
+        Passport::tokensExpireIn(CarbonInterval::minutes(15));
     }
 }
